b533a7ea65e382994de189edb3a1b75076c97815
eamon-sharma-11 <eamshaps@gmail.com>
2021-01-19 13:08:04 +1100

    Added the first GUI window

--- main.py
+++ main.py
@@ -1,1 +1,64 @@
+from tkinter import *
+import sys
+
+team_1 = ""
+
+master = Tk()
+
+
+
+
+def help_menu():
+    top = Toplevel(master)
+    top.title("Help Menu")
+    heading = Label(top, text = "Help Menu").grid(row = 0, column = 1)
+    headin_ws = Label(top, text = " ").grid(row = 1, column = 1)
+    help_1 = Label(top, text = "-This program is designed to keep track").grid(row = 2, column = 1)
+    help_1 = Label(top, text=" of scores during a cricket game").grid(row = 3, column = 1)
+    help_2 = Label(top, text = "-Any issues that you may think").grid(row = 4, column = 1)
+    help_2 = Label(top, text=" areise from the code of the game, contact").grid(row = 5, column = 1)
+    help_2 = Label(top, text=" the developer").grid(row = 6, column = 1)
+    help_3 = Label(top, text= "-Please reffer to the included").grid(row = 7, column = 1)
+    help_3 = Label(top, text=" instruction guide for more help").grid(row = 8, column = 1)
+    leave_button = Button(top, text = "EXIT", command = top.destroy).grid(row = 9, column = 1)
+
+def start():
+    top_1 = Toplevel(master)
+    top_1.title("Hows That?")
+    def submit():
+        global team_1
+        team_1 = t1_entry.get()
+    #top_1 = Frame(top_1, height=200, width=200)
+    team1_name = Label(top_1, text = "Enter Team 1's team name: ").grid(row = 0, column = 1)
+    t1_entry = Entry(top_1, width = 20)
+    t1_entry.grid(row = 1, column = 1)
+    ws_1 = Label(top_1, text = " ").grid(row = 2, column = 1)
+    submit_button = Button(top_1, text = "SUBMIT", command = submit).grid(row = 3, column = 1)
+
+
+
+
+#SETTING UP MAIN SCREEN
+welcome_label = Label(master, text = 'Welcome to "Hows That?"')
+white_space_welcome = Label(master, text = " ")
+white_space_welcome2 = Label(master, text = " ")
+white_space_welcome3 = Label(master, text = " ")
+
+help_menu_button = Button(master, text = "HELP", command = help_menu)
+start_button = Button(master, text = "START", command = start)
+exit_button = Button(master, text = "EXIT", command = sys.exit)
+
+
+welcome_label.grid(row = 0, column = 1)
+white_space_welcome.grid(row = 1, column = 1)
+white_space_welcome2.grid(row = 2, column = 1)
+white_space_welcome3.grid(row = 3, column = 1)
+help_menu_button.grid(row = 4, column = 0)
+start_button.grid(row = 4, column = 1)
+exit_button.grid(row = 4, column = 2)
+
+
+
+mainloop()
+
 
