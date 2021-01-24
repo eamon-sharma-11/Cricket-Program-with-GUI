ddbd1183c4e6a7cf04fd137fd577744c05f95b5b
eamon-sharma-11 <eamshaps@gmail.com>
2021-01-24 16:23:37 +1100

    Final confirmation for team information, will work in inputting names tmr

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 
 team_1_array= []
 double = False
+t1_amount = 0
+t2_amount = 0
 
 
 master = Tk()
@@ -63,11 +65,9 @@ def save_team2_names():
         team_2 = t2_entry.get()
         top_2.withdraw()
         confirm_screen = Toplevel(master)
-        ws_2 = Label(confirm_screen, text=" ").grid(row=1, column=0)
-        ws_2 = Label(confirm_screen, text=" ").grid(row=2, column=0)
         t1_name = Label(confirm_screen, text = "Team 1: " + team_1).grid(row = 1, column = 1)
         t2_name = Label(confirm_screen, text="Team 2: " + team_2).grid(row = 1, column = 2)
-        confirm_button = Button(confirm_screen, text = "Submit", width = 10, command = lambda: team_names(team_1)).grid(row = 4, column = 1)
+        confirm_button = Button(confirm_screen, text = "Submit", width = 10, command = team_name_1).grid(row = 4, column = 1)
         redo = Button(confirm_screen, text="Redo", width=10, command= lambda: [start_team1(), clear()]).grid(row=4, column=2)
 
 def clear():
@@ -97,48 +97,53 @@ exit_button.grid(row = 4, column = 2)
 
 ####AFTER CONFIRMING TEAMS
 
-def team_names(team):
-    global team_amount
+def team_name_1():
     global team_name_input
     global team_amount_test
     team_name_input = Toplevel(master)
     confirm_screen.withdraw()
-    enter_names = Label(team_name_input, text = "Enter amount of players in " + team).grid(row = 0, column = 1)
+    enter_names = Label(team_name_input, text = "Enter amount of players in " + team_1).grid(row = 0, column = 1)
     enter_names2 = Label(team_name_input, text="(Between 2 and 11)").grid(row=1, column=1)
     team_amount_test = Entry(team_name_input, width = 20)
     team_amount_test.grid(row = 2, column = 1)
-    submit_amount = Button(team_name_input, text="Submit", command= lambda:amount_validation(team_1_array)).grid(row=3, column=1)
-    #print(isinstance(team_amount, int))
+    submit_amount = Button(team_name_input, text="Submit", command= amount_validation_1).grid(row=3, column=1)
 
-def amount_validation(team_array):
-    team_num = 1
+
+def amount_validation_1():
     team_amount = int(team_amount_test.get())
-    if team_num == 1:
-        current_team = team_1
-    elif team_num == 2:
-        current_team = team_2
-
-
-
     if team_amount < 2 or team_amount > 11:
         error = Label(team_name_input, text = "Invalid amount, try again").grid(row = 4, column = 1)
     elif team_amount > 2 or team_amount < 11:
-        player_num = 1
-        name_input_win = Toplevel(master)
-        #name_input_win.geometry("200x150")
+        t1_amount = team_amount
+        global team_name_input2
+        global team_amount_test2
+        team_name_input2 = Toplevel(master)
         team_name_input.withdraw()
-        first = Label(name_input_win, text = "Enter player " + str(player_num) + " for " + str(current_team)).grid(row = 0, column = 1)
-        name_input = Entry(name_input_win)
-        name_input.grid(row = 1, column = 1)
-        name = str(name_input.get())
-        for i in range(0, player_num):
-            p = None
-        while not p or p in team_array:
-            #first = Label(name_input, text="Enter player " + str(player_num) + " for " + str(current_team)).grid(row=0,column=1)
-            if p in team_array:
-                error = Label(name_input, text = "Player already entered").grid(row = 2, column = 1)
-        team_array.append(p)
-        player_num = player_num + 1
+        enter_names = Label(team_name_input2, text="Enter amount of players in " + team_2).grid(row=0, column=1)
+        enter_names2 = Label(team_name_input2, text="(Between 2 and 11)").grid(row=1, column=1)
+        team_amount_test2 = Entry(team_name_input2, width=20)
+        team_amount_test2.grid(row=2, column=1)
+        submit_amount = Button(team_name_input2, text="Submit", command= final_confirm).grid(row=3, column=1)
+
+def final_confirm():
+    t2_amount = int(team_amount_test2.get())
+    if t2_amount < 2 or t2_amount > 11:
+        error = Label(team_name_input2, text = "Invalid amount, try again").grid(row = 4, column = 1)
+    elif t2_amount > 2 or t2_amount < 11:
+        final_confirm = Toplevel(master)
+        team_name_input2.withdraw()
+        con_title = Label(final_confirm, text = "Please ensure that all information is correct").grid(row = 0, column = 1)
+        t1_con = Label(final_confirm, text = str(team_1) + ": " + str(t1_amount) + " players").grid(row = 1, column = 1)
+        t2_con = Label(final_confirm, text = str(team_2) + ": " + str(t2_amount) + " players").grid(row = 2, column = 1)
+
+
+
+
+
+
+
+
+
 
 
 
