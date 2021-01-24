967a7da598761fb264ba5bf2a69ec65049764cc9
eamon-sharma-11 <eamshaps@gmail.com>
2021-01-24 12:46:44 +1100

    Added window to input amount of players on each team, error with winodow not showing

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
 import sys
 
-team_1 = ""
+team_1_array= []
 double = False
+
 
 master = Tk()
 
@@ -56,16 +57,18 @@ def start_team1():
 
 def save_team2_names():
     global team_2
+    global confirm_screen
     if t2_entry.get() == team_1:
         error = Label(top_2, text = "Invalid Team Name, try again").grid(row = 2, column = 1)
     elif t2_entry.get() != team_1:
         team_2 = t2_entry.get()
         top_2.withdraw()
-        confirm_screen = confirm_win = Toplevel(master)
+        confirm_screen = Toplevel(master)
         ws_2 = Label(confirm_screen, text=" ").grid(row=1, column=0)
         ws_2 = Label(confirm_screen, text=" ").grid(row=2, column=0)
-        t1_name = Label(confirm_screen, text = "Team 1: " + team_1).grid(row = 1, column = 2)
-        t2_name = Label(confirm_screen, text="Team 2: " + team_2).grid(row = 2, column = 2)
+        t1_name = Label(confirm_screen, text = "Team 1: " + team_1).grid(row = 1, column = 1)
+        t2_name = Label(confirm_screen, text="Team 2: " + team_2).grid(row = 2, column = 1)
+        confirm_button = Button(confirm_screen, text = "Submit", command = lambda: team_names(team_1)).grid(row = 4, column = 1)
 
 
 
@@ -93,7 +96,48 @@ exit_button.grid(row = 4, column = 2)
 
 ####AFTER CONFIRMING TEAMS
 
+def team_names(team):
+    global team_amount
+    global team_name_input
+    global team_amount_test
+    team_name_input = Toplevel(master)
+    confirm_screen.withdraw()
+    enter_names = Label(team_name_input, text = "Enter amount of players in " + team).grid(row = 0, column = 1)
+    enter_names2 = Label(team_name_input, text="(Between 2 and 11)").grid(row=1, column=1)
+    team_amount_test = Entry(team_name_input, width = 20)
+    team_amount_test.grid(row = 2, column = 1)
+    submit_amount = Button(team_name_input, text="Submit", command= lambda:amount_validation(team_1_array)).grid(row=3, column=1)
+    #print(isinstance(team_amount, int))
 
+def amount_validation(team_array):
+    team_num = 1
+    team_amount = int(team_amount_test.get())
+    if team_num == 1:
+        current_team = team_1
+    elif team_num == 2:
+        current_team = team_2
+
+
+
+    if team_amount < 2 or team_amount > 11:
+        error = Label(team_name_input, text = "Invalid amount, try again").grid(row = 4, column = 1)
+    elif team_amount > 2 or team_amount < 11:
+        player_num = 1
+        name_input_win = Toplevel(master)
+        #name_input_win.geometry("200x150")
+        team_name_input.withdraw()
+        first = Label(name_input_win, text = "Enter player " + str(player_num) + " for " + str(current_team)).grid(row = 0, column = 1)
+        name_input = Entry(name_input_win)
+        name_input.grid(row = 1, column = 1)
+        name = str(name_input.get())
+        for i in range(0, player_num):
+            p = None
+        while not p or p in team_array:
+            #first = Label(name_input, text="Enter player " + str(player_num) + " for " + str(current_team)).grid(row=0,column=1)
+            if p in team_array:
+                error = Label(name_input, text = "Player already entered").grid(row = 2, column = 1)
+        team_array.append(p)
+        player_num = player_num + 1
 
 
 
